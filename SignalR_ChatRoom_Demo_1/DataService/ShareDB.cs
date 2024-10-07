c8d8dc1708f210c3fb22ffe92ed27a44a448289b
Duong Thi Hien <134600777+DuongHienne@users.noreply.github.com>
2024-10-07 15:42:17 +0700

    first commit

---
+++ SignalR_ChatRoom_Demo_1/DataService/ShareDB.cs
@@ -0,0 +1,13 @@
+﻿using SignalR_ChatRoom_Demo_1.Models;
+using System.Collections.Concurrent;
+
+namespace SignalR_ChatRoom_Demo_1.DataService
+{
+    public class ShareDB
+    {
+        private readonly ConcurrentDictionary<string, UserConnection> _connections = new();
+
+        public ConcurrentDictionary<string, UserConnection> connections => _connections;
+
+    }
+}
---
+++ SignalR_ChatRoom_Demo_1/Hubs/ChatHub.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalR_ChatRoom_Demo_1.DataService;
+using SignalR_ChatRoom_Demo_1.Models;
+
+namespace SignalR_ChatRoom_Demo_1.Hubs
+{
+    public class ChatHub : Hub
+    {
+
+        private readonly ShareDB _shared;
+
+        public ChatHub(ShareDB shared) => _shared = shared;
+        public async Task JoinChat(UserConnection conn)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", "admin", $"{conn.Username} has joined");
+
+        }
+
+        public async Task SpecificChatRoom(UserConnection conn)
+        {
+            //add vào group theo connectionId
+            await Groups.AddToGroupAsync(Context.ConnectionId, conn.ChatRoom);
+
+            //lưu thông tin kết nối -connection
+            _shared.connections[Context.ConnectionId] = conn;
+            
+            //gửi message lên client
+            await Clients.Group(conn.ChatRoom).SendAsync("JoinSpecificChatRoom", "admin", $"{conn.Username} has joined {conn.ChatRoom}");
+
+
+        }
+
+
+        public async Task SendMessage(string msg)
+        {
+            if (_shared.connections.TryGetValue(Context.ConnectionId, out UserConnection conn))
+            {
+                await Clients.Group(conn.ChatRoom)
+                    .SendAsync("RecivedSpecificMessage", conn.Username, msg);
+            }
+        }
+
+    }
+}
